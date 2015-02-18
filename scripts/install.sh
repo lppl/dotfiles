d4d49e8731d6c9ed7dd7caa1275e48c51a01b33e
Łukasz Pietrek <lukasz.ietrek@itvp.pl>
2015-02-18 22:36:21 +0100

    Installing dotfiles

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
 
-
-
-copyDotfile () {
-
+main () {
+	local dotfiles_source_path=$(readlink  -e $(dirname $(dirname $BASH_SOURCE)))/dotfiles
+	local dotfiles_target_path="/home/lppl"
+	link_dotfiles $dotfiles_source_path $dotfiles_target_path
 }
+
+
+link_dotfiles () {
+	local source_path=$1
+	local target_path=$2
+	find $source_path -type f | while read file; do link_dotfile $file $target_path; done
+}
+
+
+link_dotfile () {
+	local source_path=$1
+	local target_path=$(readlink  -e $2)
+	local file=$(basename $source_path)
+	cp -fs "$source_path" "$target_path/.$file"
+}
+
+
+main
