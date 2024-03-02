1191e0eba7430b40ad29027f53b6f471cf87672f
Łukasz Pietrek <lpietrek@gmail.com>
2024-03-02 12:42:08 +0100

    Before rewrite

--- configfiles/nvim/init.lua
+++ configfiles/nvim/init.lua
@@ -1,12 +1,6 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -21,7 +15,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -30,8 +23,6 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -55,6 +46,7 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
+  
   { -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -135,25 +127,13 @@ require('lazy').setup({
   } ,
   -- Multicursor support
   'mg979/vim-visual-multi',
-
-
-
-
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  --
-  --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
-  --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
-  -- { import = 'custom.plugins' },
+  'ThePrimagean/git-worktree.nvim',
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  { "camgraff/telescope-tmux.nvim" }
 }, {})
 
 -- [[ Setting optio ]]
@@ -207,10 +187,12 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+vim.keymap.set({ 'n', 'v' }, ';', ':', { silent = true })
 vim.keymap.set({ 'n', 'v' }, 's', '<Nop>', { silent = true })
 vim.keymap.set({ 'n', 'v' }, 'ss', ':w<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, 'sq', ':wq<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<C-q>', ':q<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, 'sq', ':wq<cr>', { silent  = true })
+vim.keymap.set({ 'n', 'v' }, '<C-q>', ':q!<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<C-w>', ':wq<cr>', { silent = true })
 
 
 -- [[ Highlight on yank ]]
@@ -230,7 +212,6 @@ require('telescope').setup {
   defaults = {
     mappings = {
       i = {
-        ['<C-u>'] = false,
         ['<C-d>'] = false,
       },
     },
@@ -335,9 +316,6 @@ vim.g.loaded_netrwPlugin = 1
 
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
-
--- empty setup using defaults
-require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
@@ -511,11 +489,71 @@ M.general = {
   }
 }
 
+local harpoon = require("harpoon")
+harpoon:setup()
+
+
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+
+-- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+-- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+-- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+-- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-A-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-A-N>", function() harpoon:list():next() end)
+
+
+-- op = Operations.Switch, Operations.Create, Operations.Delete
+-- metadata = table of useful values (structure dependent on op)
+--      Switch
+--          path = path you switched to
+--          prev_path = previous worktree path
+--      Create
+--          path = path where worktree created
+--          branch = branch name
+--          upstream = upstream remote name
+--      Delete
+--          path = path where worktree deleted
+
+local Worktree = require("git-worktree")
+Worktree.on_tree_change(function(op, metadata)
+  if op == Worktree.Operations.Switch then
+    print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+  end
+end)
+
+require("telescope").load_extension("git_worktree")
+vim.keymap.set("n", "gwc", require('telescope').extensions.git_worktree.create_git_worktree, { desc = "[G]it [W]orktree [C]reate" })
+vim.keymap.set("n", "gww", require('telescope').extensions.git_worktree.git_worktree, { desc = "[G]it [W]orktree [W]worktree" })
+-- vim.keymap.set("n", "gws", require('telescope').extensions.git_worktree.switch_git_worktree)
+-- vim.keymap.set("n", "gwd", require('telescope').extensions.git_worktree.delete_git_worktree)
+vim.keymap.set({"n", "v"}, "<A-1>", ":NvimTreeToggle<cr>", { desc = "Toggle File Explorer" })
+vim.keymap.set({"i"}, "<A-1>", "<Esc>:NvimTreeToggle<cr>", { desc = "Toggle File Explorer" })
+
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
-
 
 vim.cmd("set background=light")
 vim.cmd('colorscheme tokyonight-moon')
---
+++ configfiles/nvim/lua/lppl/multicursor.lua
@@ -0,0 +1,10 @@
+local VimMultiCursor = {}
+
+function VimMultiCursor.lazy()
+  return 'mg979/vim-visual-multi'
+end
+
+function VimMultiCursor.setup()
+end
+
+return VimMultiCursor
