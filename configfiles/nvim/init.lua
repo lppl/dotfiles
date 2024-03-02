718125a3272258e6af6e5c791603a740d5574dd8
Łukasz Pietrek <lpietrek@gmail.com>
2024-03-02 13:59:43 +0100

    Extract navigation

--- configfiles/nvim/init.lua
+++ configfiles/nvim/init.lua
@@ -128,11 +128,7 @@ require('lazy').setup({
   -- Multicursor support
   'mg979/vim-visual-multi',
   'ThePrimagean/git-worktree.nvim',
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
+  require("lppl.navigation"),
   { "camgraff/telescope-tmux.nvim" }
 }, {})
 
@@ -176,6 +172,10 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+
+-- Theme setup
+vim.cmd("set background=light")
+vim.cmd('colorscheme tokyonight-moon')
 
 -- [[ Basic Keymaps ]]
 
@@ -489,71 +489,4 @@ M.general = {
   }
 }
 
-local harpoon = require("harpoon")
-harpoon:setup()
 
-
--- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
-
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
-end
-
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-
--- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-A-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-A-N>", function() harpoon:list():next() end)
-
-
--- op = Operations.Switch, Operations.Create, Operations.Delete
--- metadata = table of useful values (structure dependent on op)
---      Switch
---          path = path you switched to
---          prev_path = previous worktree path
---      Create
---          path = path where worktree created
---          branch = branch name
---          upstream = upstream remote name
---      Delete
---          path = path where worktree deleted
-
-local Worktree = require("git-worktree")
-Worktree.on_tree_change(function(op, metadata)
-  if op == Worktree.Operations.Switch then
-    print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
-  end
-end)
-
-require("telescope").load_extension("git_worktree")
-vim.keymap.set("n", "gwc", require('telescope').extensions.git_worktree.create_git_worktree, { desc = "[G]it [W]orktree [C]reate" })
-vim.keymap.set("n", "gww", require('telescope').extensions.git_worktree.git_worktree, { desc = "[G]it [W]orktree [W]worktree" })
--- vim.keymap.set("n", "gws", require('telescope').extensions.git_worktree.switch_git_worktree)
--- vim.keymap.set("n", "gwd", require('telescope').extensions.git_worktree.delete_git_worktree)
-vim.keymap.set({"n", "v"}, "<A-1>", ":NvimTreeToggle<cr>", { desc = "Toggle File Explorer" })
-vim.keymap.set({"i"}, "<A-1>", "<Esc>:NvimTreeToggle<cr>", { desc = "Toggle File Explorer" })
-
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
-
-vim.cmd("set background=light")
-vim.cmd('colorscheme tokyonight-moon')
---
+++ configfiles/nvim/lua/lppl/navigation.lua
@@ -0,0 +1,75 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function ()
+    local harpoon = require("harpoon")
+
+    harpoon:setup()
+
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      }):find()
+    end
+
+    vim.keymap.set("n", "sa", function() harpoon:list():append() end)
+
+    -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+
+    vim.keymap.set("n", "<C-A-0>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-A-1>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-A-2>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-A-3>", function() harpoon:list():select(4) end)
+
+    -- -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<C-A-Left>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-A-Right>", function() harpoon:list():next() end)
+
+    -- -- Toggle previous & next buffers nvim buffers 
+    vim.keymap.set("n", "<C-A-S-Left>", ":bn<cr>")
+    vim.keymap.set("n", "<C-A-S-Right>", ":bp<cr>")
+  end
+}
+
+
+-- -- op = Operations.Switch, Operations.Create, Operations.Delete
+-- -- metadata = table of useful values (structure dependent on op)
+-- --      Switch
+-- --          path = path you switched to
+-- --          prev_path = previous worktree path
+-- --      Create
+-- --          path = path where worktree created
+-- --          branch = branch name
+-- --          upstream = upstream remote name
+-- --      Delete
+-- --          path = path where worktree deleted
+--
+-- local Worktree = require("git-worktree")
+-- Worktree.on_tree_change(function(op, metadata)
+--   if op == Worktree.Operations.Switch then
+--     print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+--   end
+-- end)
+--
+-- require("telescope").load_extension("git_worktree")
+-- vim.keymap.set("n", "gwc", require('telescope').extensions.git_worktree.create_git_worktree, { desc = "[G]it [W]orktree [C]reate" })
+-- vim.keymap.set("n", "gww", require('telescope').extensions.git_worktree.git_worktree, { desc = "[G]it [W]orktree [W]worktree" })
+-- -- vim.keymap.set("n", "gws", require('telescope').extensions.git_worktree.switch_git_worktree)
+-- -- vim.keymap.set("n", "gwd", require('telescope').extensions.git_worktree.delete_git_worktree)
+-- vim.keymap.set({"n", "v"}, "<A-1>", ":NvimTreeToggle<cr>", { desc = "Toggle File Explorer" })
+-- vim.keymap.set({"i"}, "<A-1>", "<Esc>:NvimTreeToggle<cr>", { desc = "Toggle File Explorer" })
+--
+--
