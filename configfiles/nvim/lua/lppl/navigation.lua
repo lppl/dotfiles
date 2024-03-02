258a5feb450aac925e64c29b6107bd1bf3a105a2
Łukasz Pietrek <lpietrek@gmail.com>
2024-03-02 14:55:51 +0100

    Update navigation

--- configfiles/nvim/lua/lppl/navigation.lua
+++ configfiles/nvim/lua/lppl/navigation.lua
@@ -1,5 +1,54 @@
 return {
   {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+      require('telescope').setup {
+        defaults = {
+          mappings = {
+            i = {
+              -- ['<C-d>'] = false,
+            },
+          },
+        },
+      }
+
+      pcall(require('telescope').load_extension, 'fzf')
+
+      vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+      vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>/', function()
+        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] Fuzzily search in current buffer' })
+
+      vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+      vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
+      vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
+      vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+      vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+
+      vim.keymap.set('n', 'sf', require('telescope.builtin').find_files, { desc = '[S]earch [f]iles' })
+      vim.keymap.set('n', 'sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', 'sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', 'sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', 'sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+    end
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    -- NOTE: If you are having trouble with this installation,
+    --       refer to the README for telescope-fzf-native for more instructions.
+    build = 'make',
+    cond = function()
+      return vim.fn.executable 'make' == 1
+    end,
+  },
+  {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -23,12 +72,12 @@ return {
           previewer = conf.file_previewer({}),
           sorter = conf.generic_sorter({}),
         }):find()
-      end
+     end
 
-      vim.keymap.set("n", "sa", function() harpoon:list():append() end)
+      vim.keymap.set("n", "<leader>ma", function() harpoon:list():append() end, { desc = "Harpoon [M]ark [A]ppend" })
+      vim.keymap.set("n", "<leader>mr", function() harpoon:list():remove() end, { desc = "Harpoon [M]ark [R]emove" })
 
-      -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-      vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+      vim.keymap.set("n", "<leader>mm", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
 
       vim.keymap.set("n", "<C-A-0>", function() harpoon:list():select(1) end)
       vim.keymap.set("n", "<C-A-1>", function() harpoon:list():select(2) end)
