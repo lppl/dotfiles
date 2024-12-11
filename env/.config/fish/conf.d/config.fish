9b26a1fc093ea8655ef2edf9195a44787361e75c
Łukasz Pietrek <lpietrek@gmail.com>
2024-12-11 15:34:48 +0100

    [DEV-UPDATE]: fish

--- env/.config/fish/conf.d/config.fish
+++ env/.config/fish/conf.d/config.fish
@@ -1,5 +1,10 @@
 set -gx DEV_ENV_DIR "$HOME/utils/dotfiles"
+set -gx QMK_HOME "$HOME/utils/qmk_firmware"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx PATH "$HOME/bin" "$HOME/.local/bin" $PATH
+
 set -gx EDITOR vim
 set -gx VISUAL vim
+
+
+fish_vi_key_bindings
