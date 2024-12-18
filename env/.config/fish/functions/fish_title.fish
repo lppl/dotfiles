7da75670c2dfd0ff4e9e7aad9cc9345c114207fb
Łukasz Pietrek <lpietrek@gmail.com>
2024-12-18 20:41:28 +0100

    [DEV-UPDATE]: fish

--- env/.config/fish/functions/fish_title.fish
+++ env/.config/fish/functions/fish_title.fish
@@ -10,6 +10,25 @@ function __bobthefish_title_user -S -d 'Display actual user if different from $d
     end
 end
 
+# function fish_title
+#     __bobthefish_title_user
+#
+#     if [ "$theme_title_display_process" = 'yes' ]
+#         status current-command
+#
+#         [ "$theme_title_display_path" != 'no' ]
+#         and echo ' '
+#     end
+#
+#     if [ "$theme_title_display_path" != 'no' ]
+#         if [ "$theme_title_use_abbreviated_path" = 'no' ]
+#             echo $PWD
+#         else
+#             prompt_pwd
+#         end
+#     end
+# end
+
 function fish_title
     __bobthefish_title_user
 
