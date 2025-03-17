f6cf5ae6d3a1ca5927b8d00edb5c481a94f38949
Łukasz Pietrek <lpietrek@gmail.com>
2025-03-17 13:40:28 +0100

    [main] dotfiles dev update: env/.config/nvim

--- env/.config/nvim/lua/lppl/ai-codecompanion.lua
+++ env/.config/nvim/lua/lppl/ai-codecompanion.lua
@@ -14,7 +14,20 @@ return {
     { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
   },
   config = function()
-    require("codecompanion").setup()
+    require("codecompanion").setup({
+      display = {
+        action_palette = {
+          width = 95,
+          height = 10,
+          prompt = "Prompt ",                   -- Prompt used for interactive LLM calls
+          provider = "telescope",               -- default|telescope|mini_pick
+          opts = {
+            show_default_actions = true,        -- Show the default actions in the action palette?
+            show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+          },
+        },
+      },
+    })
     nmap("<leader>cl", "<cmd>CodeCompanionActions<cr>", "Code Companion Actions")
     nmap("<leader>cc", "<cmd>CodeCompanionChat<cr>", "Code Companion Chat")
     nmap("<leader>cj", "<cmd>CodeCompanionChat Add<cr>", "Code Companion Add to Chat")
