4f11a68a19aef77ce5bc5db313bcd2a5dd10ba22
Łukasz Pietrek <lpietrek@gmail.com>
2025-10-08 20:50:00 +0200

    Use different cursor carets to indicate vi mode

--- env/.config/fish/conf.d/config.fish
+++ env/.config/fish/conf.d/config.fish
@@ -6,5 +6,10 @@ set -gx PATH "$HOME/bin" "$HOME/.local/bin" $PATH
 set -gx EDITOR vim
 set -gx VISUAL vim
 
-
 fish_vi_key_bindings
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual blockacter
