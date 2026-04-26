d7ae6735c4682fcc3480c62b5da057ed690f9aeb
Łukasz Pietrek <lpietrek@gmail.com>
2026-04-26 23:06:18 +0200

    Remove nvim

--- env/.config/nvim/lua/config/plugins/mini.nvim.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'echasnovski/mini.ai',
-  'echasnovski/mini.align',
-}
--- env/.config/nvim/lua/config/user/commands.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_create_user_command('Reboot', 'qa!', {})
-vim.api.nvim_create_user_command('R', 'qa!', {})
