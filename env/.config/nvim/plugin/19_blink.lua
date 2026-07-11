bb042b087ad547db8238d6c046f1b4b3a58a58c9
Łukasz Pietrek <lpietrek@gmail.com>
2026-07-11 09:40:42 +0200

    Update main

---
+++ env/.config/nvim/plugin/19_blink.lua
@@ -0,0 +1,15 @@
+vim.g.completion_mode = "blink"
+
+vim.pack.add {
+  { src = "https://github.com/Saghen/blink.cmp", version = "v1.10.2" },
+}
+
+require("blink.cmp").setup {
+  keymap = { preset = "default" },
+  appearance = { nerd_font_variant = "mono" },
+  completion = { documentation = { auto_show = false } },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+  },
+  fuzzy = { implementation = "prefer_rust_with_warning" },
+}
